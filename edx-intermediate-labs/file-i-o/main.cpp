9df0d33c07f909a27f2b16d0fbc68885b39a7cb8
William Chislett <wchislett@sorensonmedia.com>
2017-08-16 18:01:40 +0100

    Adding 4th lab exercise - Edx c++.  Basic file IO.

---
+++ edx-intermediate-labs/file-i-o/main.cpp
@@ -0,0 +1,10 @@
+#include <fstream>
+
+using namespace std;
+
+int main () {
+    string nameAndAmount;
+    cout << "Enter donation name and amount" << endl;
+    cin >> nameAndAmount;
+    return 0;
+}
