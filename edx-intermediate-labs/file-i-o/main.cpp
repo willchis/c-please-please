801259a4159c42489d728619071b68803193c2bf
William Chislett <willchis@hotmail.com>
2017-08-16 23:11:03 +0100

    Accepting user input and appending to txt file.

--- edx-intermediate-labs/file-i-o/main.cpp
+++ edx-intermediate-labs/file-i-o/main.cpp
@@ -1,10 +1,31 @@
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
 int main () {
-    string nameAndAmount;
-    cout << "Enter donation name and amount" << endl;
-    cin >> nameAndAmount;
+
+    fstream donationFile("donation_total.txt", ios_base::app);
+
+    if (donationFile.is_open())
+    {
+        // File opened OK, so use it…
+        cout << "File open" << endl;
+
+        string nameAndAmount;
+        cout << "Enter donation name and amount: ";
+        cin >> nameAndAmount;
+
+        donationFile << nameAndAmount << endl;
+
+        cout << "You entered: " << nameAndAmount << endl;
+        
+        // Close the file when we're done.
+        donationFile.close();
+    }
+    else
+    {
+        cerr << "Couldn't open file" << endl;
+    }
     return 0;
 }
