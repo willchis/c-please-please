d4c354f0a28f8558a1d7cfd48956f45113e59be2
William Chislett <wchislett@sorensonmedia.com>
2017-10-18 13:04:49 +0100

    Adding pseudo-code for heap sort (edx course).

---
+++ algorithm-course/HeapSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Min heap - smallest at top
+void heapify(int theNumbers[])
+{
+    // Start at one (parent)
+    // if left-child < parent
+      // swap left-child and parent
+    // if right-child < parent (which could now be the old left-child)
+      // swap right-child and parent
+      
+}
+
+int main() 
+{
+    cout << "Foo" << endl;
+}
