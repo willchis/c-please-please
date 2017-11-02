0b1519192222a4c54c441761af765b49562a0197
William Chislett <wchislett@sorensonmedia.com>
2017-11-02 11:10:26 +0000

    Finishing merge sort algo.

--- algorithm-course/MergeSort.cpp
+++ algorithm-course/MergeSort.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
 #include <vector>
 
-// NOTE: this is also saved in a repl.it example.
-
 using namespace std;
 
-vector<int> mergeSort(vector<int> arrayToSort) {
+vector<int> combine(vector<int> left, vector<int> right) {
+    cout << "...." << endl;
+    vector<int> combined;
+    
+    int l = 0;
+    int r = 0;
+    
+    while (l < left.size() && r < right.size()) {
+        if (left[l] <= right[r]) {
+            combined.push_back(left[l]);
+            l++;
+        } else {
+            combined.push_back(right[r]);
+            r++;
+        }
+    }
+    
+    // remaining elements
+    while (l < left.size()) {
+        combined.push_back(left[l]);
+        l++;
+    }
+    while (r < right.size()) {
+        combined.push_back(right[r]);
+        r++;
+    }
+    
+    for(int i = 0; i < combined.size(); i++) {
+        cout << combined[i] << ", ";
+    }
+    
+    cout << endl << "...." << endl;
+    return combined;
+}
+
+vector<int> split(vector<int> arrayToSort) {
     for(int i = 0; i < arrayToSort.size(); i++) {
         cout << arrayToSort[i] << ", ";
     }
@@ -16,16 +49,18 @@ vector<int> mergeSort(vector<int> arrayToSort) {
     } else {
         vector<int> left(arrayToSort.begin(), arrayToSort.begin() + arrayToSort.size()/2);
         vector<int> right(arrayToSort.begin() + arrayToSort.size()/2, arrayToSort.end());
-        mergeSort(left);
-        mergeSort(right);
+        
+        arrayToSort = combine(split(left), split(right));
     }
     
     return arrayToSort;
 }
 
+
+
 int main() {
-    vector<int> testArray = { 4, 6, 9, 2, 1, 8 };
+    vector<int> testArray = { 4, 6, 9, 2, 1, 8, 10, 3, 88, 4, 6 };
     
-    mergeSort(testArray);
+    split(testArray);
     
 }
