30a18f89ef4750befa16ed419ba1a62e9db412e3
willchis <willchis@hotmail.com>
2022-11-29 20:44:47 -0700

    text padding function

--- learncpp.com/main.cpp
+++ learncpp.com/main.cpp
@@ -2,20 +2,44 @@
 #include <fstream>
 #include <ctime>
 
+void padText(char text[], int textLength, char padCharacter, int padCount) {
+    char tempText[textLength];
+    char tempChar = text[0];
+
+    for (int i = 0; i < textLength; i++) {
+        tempText[i] = text[i];
+        if (i < padCount) {
+            text[i] = padCharacter;
+        }
+    }
+
+    for (int i = 0; i < textLength; i++) {
+        if (i == textLength - 1) {
+            text[i] = '\0';
+        } else if (i >= padCount) {
+            if (tempText[i - padCount] == '\0') {
+                break;
+            }
+            text[i] = tempText[i - padCount];
+        }
+
+    }
+}
+
 int main()
 {
-    int x = 5;
-    std::string userText; 
-    std::cout << x << ">>"; // print value of x (5) to console
-    std::cin >> userText;
-
     std::time_t t = std::time(0);
     std::tm* now = std::localtime(&t);
-    std::cout << (now->tm_year + 1900) << '-' 
-         << (now->tm_mon + 1) << '-'
-         <<  now->tm_mday
-         << "\n";
-         
+    std::string today = std::to_string(now->tm_year + 1900) 
+        + '-' + std::to_string(now->tm_mon + 1) 
+        + '-' + std::to_string(now->tm_mday);
 
+    char test[] = "tesonetw";
+    int length = sizeof(test) / sizeof(char);
+
+    padText(test, length, ' ', 5);
+
+    std::cout << "result: " << test;
     return 0;
 }
+
