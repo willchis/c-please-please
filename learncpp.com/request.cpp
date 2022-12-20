9b2e039eb94c21e0f6f8b8ddcd775f1e689076dc
willchis <3491130+willchis@users.noreply.github.com>
2022-12-19 20:37:42 -0700

    parse json response and output

--- learncpp.com/request.cpp
+++ learncpp.com/request.cpp
@@ -2,22 +2,46 @@
 #include <curl/curl.h>
 #include "json.hpp"
 
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+
+static size_t
+WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *data)
+{
+    ssize_t realsize = size * nmemb;
+
+    std::string* mem = reinterpret_cast<std::string*>(data);
+    mem->append(static_cast<char*>(contents), realsize);
+
+    return realsize;
+}
+
 void get_request(std::string url) {
     CURLcode res;
 
     CURL *curl = curl_easy_init();
     if(curl) {
+        std::string data;
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+        std::cout << data << std::endl;
 
         /* Perform the request, res will get the return code */
         res = curl_easy_perform(curl);
         /* Check for errors */
         if(res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        } else {
+            if (data.size() > 0) {
+                nlohmann::json weather_data = nlohmann::json::parse(data);
+                std::cout << weather_data["main"]["temp"] << std::endl;
+            }
         }
 
         /* always cleanup */
         curl_easy_cleanup(curl);
     }
-    //return 0;
 }
